2c8012716ae91b8c0bcc636a8ced0aba4e02e1a8
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-12-23 12:19:09 -0500

    Orientado en terminos del dominio

--- platzi-market/src/main/java/com/platzi/platzi_market/persistence/mapper/ProductMapper.java
+++ platzi-market/src/main/java/com/platzi/platzi_market/persistence/mapper/ProductMapper.java
@@ -11,20 +11,17 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
-
     @Mappings({
             @Mapping(source = "idProducto", target = "productId"),
             @Mapping(source = "nombre", target = "name"),
             @Mapping(source = "idCategoria", target = "categoryId"),
             @Mapping(source = "precioVenta", target = "price"),
             @Mapping(source = "cantidadStock", target = "stock"),
-            @Mapping(source = "idProducto", target = "productoId"),
             @Mapping(source = "estado", target = "active"),
             @Mapping(source = "categoria", target = "category"),
-
     })
     Product toProduct(Producto producto);
-    List<Product> toProduct(List<Product> productos);
+    List<Product> toProducts(List<Producto> productos);
 
     @InheritInverseConfiguration
     @Mapping(target = "codigoBarras", ignore = true)
