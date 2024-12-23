477123dcf2c0b84d2b56a00a16c3976259b54306
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-12-23 12:07:20 -0500

    MapStruct

---
+++ platzi-market/src/main/java/com/platzi/platzi_market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.platzi.platzi_market.persistence.mapper;
+
+import com.platzi.platzi_market.domain.Product;
+import com.platzi.platzi_market.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "idProducto", target = "productoId"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
+
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProduct(List<Product> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+}
