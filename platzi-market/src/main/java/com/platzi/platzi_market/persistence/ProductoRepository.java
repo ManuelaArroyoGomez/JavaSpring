25ae74fb729208a845733fe5b1c343f749edcb28
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-12-23 11:22:29 -0500

    @Repository

--- platzi-market/src/main/java/com/platzi/platzi_market/persistence/ProductoRepository.java
+++ platzi-market/src/main/java/com/platzi/platzi_market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.platzi_market.persistence;
 
 import com.platzi.platzi_market.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzi_market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -20,5 +22,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    // Obtener un producto con el id
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    // Guardar y eliminar
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
